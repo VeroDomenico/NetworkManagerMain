25866838d3809b94d0970c54a12103527c223c2f
Dominic Meconi <meconidominic@gmail.com>
2023-04-20 18:02:36 -0500

    adjustments

--- Server/internal/Auth/auth.go
+++ Server/internal/Auth/auth.go
@@ -2,29 +2,80 @@ package auth
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 //Check session
 
-func authenticateLogin(w http.ResponseWriter, r *http.Request) {
+func authenticateLogin(w http.ResponseWriter, r *http.Request) (http.ResponseWriter, *http.Request) {
 	var LoginRequest struct {
 		Username string `json:"username"`
-		Password string `json:"Password"`
+		Password string `json:"password"`
 	}
 	//Check request is valid to be decoded in bson for go
 	if json.NewDecoder(r.Body).Decode(&LoginRequest) != nil {
+		fmt.Println("Unable to decode login Request")
 		http.Error(w, "Invalid Request", http.StatusBadRequest)
+		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	//Authenticate authenticity of the user
-	//TODO
+	//validate credentials against DB
+	valid := !validateCredentials(LoginRequest.Username, LoginRequest.Username)
+
+	if valid {
+		generateLoginToken(LoginRequest.Username)
+	} else {
+
+		w.WriteHeader(http.StatusUnauthorized)
+		return w, r
+	}
 
 	w.WriteHeader(http.StatusOK)
-
+	return w, r
 }
 
 // TODO
 // func authorizeAccess() {
 
 // }
+
+func validateCredentials(username string, password string) bool {
+
+	// Get the user from the database.
+	user, err := db.GetUser(username)
+	if err != nil {
+		// The user does not exist.
+		return false
+	}
+
+	// Compare the user's password with the password that was submitted.
+	if user.Password != password {
+		// The password is incorrect.
+		return false
+	}
+
+	// The user's credentials are valid.
+	return true
+}
+
+// Used for authorizaiton TODO
+func generateLoginToken(username string) string {
+
+	// Generate a random token.
+	// token := uuid.New().String()
+
+	// Save the token to the database.
+	// err := db.SaveLoginToken(username, token)
+	// if err != nil {
+	// 	// There was an error saving the token to the database.
+	// 	return ""
+	// }
+
+	// The token was generated successfully.
+	// return token
+	return ""
+}
+
+//check token
--- Server/main.go
+++ Server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -15,7 +17,7 @@ func main() {
 	r := mux.NewRouter()
 
 	//Route Handlers
-	r.HandleFunc("/login", login)
+	r.HandleFunc("/login", login).Methods("POST")
 
 	//
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -24,5 +26,34 @@ func main() {
 
 }
 func login(w http.ResponseWriter, r *http.Request) {
+	type LoginRequest struct {
+		Username string `json: "username"`
+		Password string `json: "password"`
+	}
+	var loginReq LoginRequest
+
+	// Attempt to decode Response
+	err := json.NewDecoder(r.Body).Decode(&loginReq)
+	if err != nil {
+		fmt.Println("Unable to decode login Request")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//validate credentials against DB
+	valid, err := !validateCredentials(loginReq.Username, loginReq.Password)
+	if err != nil {
+
+	}
+	token := generateLoginToken(loginReq.Username)
+
+	w.Write([]byte(token))
+}
+
+func validateCredentials(username string, password string) (bool err) {
+
+}
+
+func generateLoginToken(username string) string {
 
 }
